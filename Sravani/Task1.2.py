4ab483278bbc4caf06cb60217acb8db48faa8b8c
Sravani Malekar <60072148+SravaniMalekar@users.noreply.github.com>
2020-10-27 04:08:52 +0530

    Update Task1.2.py

--- Sravani/Task1.2.py
+++ Sravani/Task1.2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+# Importing Libraries
 import rospy
 from geometry_msgs.msg import Twist, Point
 from sensor_msgs.msg import LaserScan
@@ -8,58 +9,54 @@ from tf.transformations import euler_from_quaternion
 import numpy as np
 import math
 
-x = 0
-y = 0
-theta = 0
-final_x = 12.5
-final_y = 0
-position_ = Point()
-initial = Point()
+# Variables
+x, y, theta, final_y, final_x = 0, 0, 0, 0, 12.5
+
+position_, initial = Point(), Point()
 
 
-def main():
-    global pub, goal, rate
-    global velocity_msg
+# Control Loop - Initialising Loop
+def control_loop():
+    # Setting Global Variables
+    global pub, goal, rate, velocity_msg
+
     velocity_msg = Twist()
-
-    velocity_msg.linear.x = 0
-    velocity_msg.angular.z = 0
     goal = Point()
-    state = 0
-    rospy.init_node('reading_laser')
-    rate = rospy.Rate(10)
+
+    # Initializing Node
+    rospy.init_node('ebot_controller')
+
+    # Setting Publisher and Subscriber
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
     rospy.Subscriber('/ebot/laser/scan', LaserScan, laser_callback)
     rospy.Subscriber('/odom', Odometry, odom_callback)
-    control_loop(1)
+
+    # Setting Rate
+    rate = rospy.Rate(10)
+
+    # Calling Path Tracing Algorithm
+    path_tracing(1)
+
     rospy.spin()
-    pub.publish(velocity_msg)
 
-
-def Waypoint(t):
+# WayPoint - A function to set the net waypoint
+def waypoint(t):
     x = ((math.pi*t)/180)
     y = (2*(math.sin(x))*(math.sin(x/2)))
     return [round(x, 1), round(y, 1)]
 
-
+# Odometry Callback - A callback function used to extract the information from the subscribed topic
 def odom_callback(msg):
-    global pose
-    global x
-    global y
-    global theta
-    global position_
+    global x, y, theta, position_
 
-    # position
     position_ = msg.pose.pose.position
     x = round(msg.pose.pose.position.x, 1)
     y = round(msg.pose.pose.position.y, 1)
     rot_q = msg.pose.pose.orientation
-    pose = (roll, pitch, th) = euler_from_quaternion(
-        [rot_q.x, rot_q.y, rot_q.z, rot_q.w])
-
+    th = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])[2]
     theta = round(th, 1)
 
-
+# Laser Callback - A callback function used to extract the information from the subscribed topic
 def laser_callback(msg):
     global regions
     regions = {
@@ -69,194 +66,173 @@ def laser_callback(msg):
         'fleft':  min(min(msg.ranges[435:579]), 20),
         'left':   min(min(msg.ranges[580:720]), 20),
     }
-    rospy.loginfo(regions)
 
-
-def go_to_waypoint(final_x, final_y):
+# Go To Goal: A function to go to a desired point in the x-y plane
+def go_to_goal(final_x, final_y):
+    # Calculating difference in current and desired points
     inc_x = final_x - x
     inc_y = final_y - y
+
+    # Calculating angle towards the goal
     angle_to_goal = round(math.atan2(inc_y, inc_x), 1)
+
+    # P only Controller
     diff = angle_to_goal-theta
     p = 1.1
     error = p*diff
+
+    # Setting tolerance
     theta_prec = math.pi/90
     dist_prec = 0.3
+
+    # Calculating distance between current and desired points
     err_pos = math.sqrt(pow(final_y - y, 2) + pow(final_x - x, 2))
-    print "error=="+str(error)
-    print "theta=="+str(theta)
-    print "angle=="+str(angle_to_goal)
 
+    # Turning Anti-clockwise
     if (error) > theta_prec:
-
         velocity_msg.linear.x = 0.0
-        velocity_msg.angular.z = 0.9 + error
-    # turn clockwise
+        velocity_msg.angular.z = 0.7 + error
+
+    # Turning Clockwise
     elif (error) < -theta_prec:
-
         velocity_msg.linear.x = 0.0
-        velocity_msg.angular.z = -0.9 + error
+        velocity_msg.angular.z = -0.7 + error
 
-    # move forward
+    # Moving Forward
     elif (err_pos > dist_prec):
-
         velocity_msg.linear.x = 0.9
         velocity_msg.angular.z = 0.0
-    # stop
-    elif (err_pos < 0.2):
 
+    # Stopping Condition
+    elif (err_pos < 0.2):
         velocity_msg.linear.x = 0.0
         velocity_msg.angular.z = 0.0
+
+    # Publishing Angular and Linear Velocities
     pub.publish(velocity_msg)
 
-
+# Obstacle Avoider - A function to avoid any obstacle in path
 def obstacle(regions):
-    print("Obstacle loop")
     velocity_msg.linear.x = 0
     velocity_msg.angular.z = 0
-    state_description = ''
-    d = 1.5
-    if regions['front'] > d and regions['fleft'] > d and regions['left'] > d and regions['right'] > d:
-        state_description = ' Straight'
-        velocity_msg.linear.x = 0.7
-        velocity_msg.angular.z = 0
-    elif regions['front'] < d and regions['left'] > d and regions['right'] > d and regions['fleft'] < d:
-        state_description = ' Left'
+    d = 1.2
+    # front object and left object
+    if regions['front'] < d and regions['fleft'] < d :
         velocity_msg.linear.x = 0
         velocity_msg.angular.z = 0.55
-    elif regions['front'] < d and regions['left'] > d and regions['fright'] < d and regions['fleft'] > d:
-        state_description = ' more Left'
-        velocity_msg.linear.x = 0.12
-        velocity_msg.angular.z = 0.57
-    elif regions['front'] > d and regions['fleft'] > d and regions['fright'] < d:
-        state_description = ' left straight'
-        velocity_msg.linear.x = 0.8
-        velocity_msg.angular.z = 0
-    elif regions['front'] > d and regions['left'] > d and regions['fleft'] > d and regions['right'] < d:
-        state_description = ' Rgt straight'
-        velocity_msg.linear.x = 0.3
-        velocity_msg.angular.z = -0.68
-    #elif regions['front'] > d and regions['fleft'] > d and regions['left'] > d and regions['right'] < d:
-    #     state_description = 'Goal'
-    #     while True:
-    #         goal(12.5, -1)
+        print(regions['front'],regions['fleft'])   
+    # right obtacle far away
+    elif regions['fright'] > d :
+        velocity_msg.linear.x = 0.25
+        velocity_msg.angular.z = -0.6
+        print(regions['fright'])
+    #right obstacle very close 
+    elif regions['fright'] < d :
+        velocity_msg.linear.x = 0.25
+        velocity_msg.angular.z = 0.6
+        print(regions['fright'])
+    #rigjt object very very far
+    elif (regions['fright'] > (1.1)*d) and (regions['fleft'] > 5*d):
+        velocity_msg.linear.x = 0.4
+        velocity_msg.angular.z = -0.83
+        print(regions['fright'],regions['fleft'])
 
-
-    # rospy.loginfo(regions)
-    # rospy.loginfo(state_description)
-    # msg.linear.x = velocity_msg.linear.x
-    # msg.angular.z = angular_z
+    # Publishing Angular and Linear Velocities
     pub.publish(velocity_msg)
 
 
-def change_state(state):
-    global state_
-    state_ = state
-    if state_ == 0:
-        go_to_waypoint(final_x, final_y)
-    elif state_ == 1:
-        obstacle(regions)
-
-
+# Distance To Line - A function to calculate the distance to the imaginary line (line between the initial point and the goal point)
 def distance_to_line(p0):
     # p0 is the current position
-    # p1 and p2 points define the line
+    # p1 and p2 points define the imaginary line
     p1 = initial
     p2 = Point()
     p2.x = final_x
     p2.y = final_y
-    # here goes the equation
+
+    # Calculation of the distance to the line
     up_eq = math.fabs((p2.y - p1.y) * p0.x - (p2.x - p1.x)
                       * p0.y + (p2.x * p1.y) - (p2.y * p1.x))
     lo_eq = math.sqrt(pow(p2.y - p1.y, 2) + pow(p2.x - p1.x, 2))
     distance = up_eq / lo_eq
-    print(p1)
-    print(p2)
+
     return distance
 
-
-def control_loop(pangle):
-    global regions
+# Path Tracing - A function to make the bot follow a given path using the generated waypoints
+def path_tracing(pangle):
 
     while not rospy.is_shutdown():
-        # Your algorithm to complete the obstacle course
-
         for t in [pangle]:
-            [goal.x, goal.y] = Waypoint(t)
+            # Generating the next waypoint
+            [goal.x, goal.y] = waypoint(t)
+
+            # Calculating difference in current and desired points
             inc_x = goal.x - x
             inc_y = goal.y - y
+
+            # Calculating angle towards the goal
             angle_to_goal = round(math.atan2(inc_y, inc_x), 1)
-            print("inside loop", pangle)
-            print("x = {}".format(x))
-            print("y = {}".format(y))
-            print("goal.x = {}".format(goal.x))
-            print("goal.y = {}".format(goal.y))
+
+            # P only Controller
             diff = angle_to_goal-theta
             p = 1.1
             error = p*diff
+
+            # Tolerance
             theta_prec = math.pi/90
             dist_prec = 0.3
+
+            # Calculating distance between current and desired points
             err_pos = math.sqrt(pow(goal.y - y, 2) + pow(goal.x - x, 2))
 
-            # print("theta = {}".format(theta))
-            # print("angle_to_goal = {}".format(angle_to_goal))
-
-            # turn anticlockwise
+            # Turning Anti-clockwise
             if (error) > theta_prec:
-
                 velocity_msg.linear.x = 0.0
                 velocity_msg.angular.z = 0.7 + error
-            # turn clockwise
-            elif (error) < -theta_prec:
 
+            # Turning Clockwise
+            elif (error) < -theta_prec:
                 velocity_msg.linear.x = 0.0
                 velocity_msg.angular.z = -0.7 + error
-                # move forward
+
+            # Moving Forward
             elif (err_pos > dist_prec):
-                # else:
                 velocity_msg.linear.x = 0.7
                 velocity_msg.angular.z = 0.0
-            # checking and generating waypoints
+
+            # Checking and Generating Waypoints
             elif (err_pos < dist_prec):
-                # elif(x == goal.x):
+                # Before 2pi [Path Tracing]
                 if (pangle < 360):
-                    control_loop(pangle+45)
-                # stop
+                    path_tracing(pangle+45)
+
+                # After 2pi [Obstacle Avoidance]
                 else:
+                    # Setting Initial Point
                     initial.x = x
                     initial.y = y
                     d = 1.85
-                    # velocity_msg.linear.x = 0
-                    # velocity_msg.angular.z = 0
-                    
-                    
-                    print("inside Control wala while loop")
+
                     while True:
+                        # Calculating distance to the imaginary line
                         distance_position_to_line = distance_to_line(position_)
-                        print str(distance_position_to_line)
-                        #if (regions['front'] > d and regions['fleft'] > d and regions['fright'] > d)or (regions['front'] > d and regions['left'] > d and regions['right'] < d) or (regions['front'] > d and regions['left'] < d and regions['right'] > d) and distance_position_to_line < 0.1:
-                        if distance_position_to_line < 1.0 and (regions['front'] > d and regions['fleft'] > d ) :
-                        #or (regions['left'] > d and regions['fright'] < d/2 and regions['right'] < d/2):
-                            # WAYPOINT
-                            change_state(0)
-                            print("state changed to 0")
-                            print str(distance_position_to_line)
 
-                        elif (regions['front'] < d and regions['fleft'] < d and regions['fright'] < d) or distance_position_to_line > 0.1 :
-                            # OBSTACLE AVOIDER
-                            change_state(1)
-                            print str(distance_position_to_line)
-                            print("state changed to 1")
+                        # Condition for Go To Goal
+                        if distance_position_to_line < 0.3 and (regions['front'] > d and regions['fleft'] > d):
+                            go_to_goal(final_x, final_y)
 
+                        # Condition for Obstacle Avoider
+                        elif (regions['front'] < d and regions['fleft'] < d and regions['fright'] < d) or distance_position_to_line > 0.1:
+                            obstacle(regions)
 
+        # Publishing Angular and Linear Velocities
         pub.publish(velocity_msg)
-        print("#############################################################")
         print("Controller message pushed at {}".format(rospy.get_time()))
-        print("#############################################################")
         rate.sleep()
 
 
 if __name__ == '__main__':
     try:
-        main()
+        control_loop()
     except rospy.ROSInterruptException:
         pass

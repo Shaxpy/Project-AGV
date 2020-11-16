c7cbdbe45f9786479deb5d4d0bb4e461821694af
akhiljayan29aj <akhiljayan29.aj@gmail.com>
2020-11-16 13:52:32 +0530

    ebot_nav package updated

--- packages/ebot_nav/scripts/seq.py
+++ packages/ebot_nav/scripts/seq.py
@@ -1,57 +1,60 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2.7
 
 import rospy
-
-# Brings in the SimpleActionClient
+# For using SimpleActionClient
 import actionlib
 # Brings in the .action file and messages used by the move base action
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 
-def movebase_client(x, y):
+def client_coordinates(x, y):
 
-    # Create an action client called "move_base" with action definition file "MoveBaseAction"
+    # Initializing action client "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
 
-# Waits until the action server has started up and started listening for goals.
+# Waiting for action server and listening for goals.
     client.wait_for_server()
 
-# Creates a new goal with the MoveBaseGoal constructor
-
+# New goal with the MoveBaseGoal constructor
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    # Move 0.5 meters forward along the x axis of the "map" coordinate frame
+    # Move forward along the x axis of the "map" coordinate frame
     goal.target_pose.pose.position.x = x
     goal.target_pose.pose.position.y = y
 
-    # No rotation of the mobile base frame w.r.t. map frame
     goal.target_pose.pose.orientation.w = 2.0
 
-    # Sends the goal to the action server.
+    # Sending the goal to the action server.
     client.send_goal(goal)
-    # Waits for the server to finish performing the action.
+    # Wait for the server to finish performing the action.
     wait = client.wait_for_result()
-    # If the result doesn't arrive, assume the Server is not available
+    # If the result doesn't exist, assume no Server is available
     if not wait:
-        rospy.logerr("Action server not available!")
-        rospy.signal_shutdown("Action server not available!")
+        rospy.logerr("No Action server !")
+        rospy.signal_shutdown("No Action server !")
     else:
         # Result of executing the action
         return client.get_result()
 
 
-# If the python node is executed as main process (sourced directly)
+def waypoints():
+        # Rospy node for SimpleActionClient to publish and subscribe
+    rospy.init_node('movebase_client_py')
+    points_seq = [-9.1, -1.2, 10.7, 10.5,
+                      12.6, -1.7, 18.2, -1.4, -2.0, 4.0]
+    for i in [0, 2, 4, 6, 8]:
+        result = client_coordinates(points_seq[i], points_seq[i+1])
+    return result
+
+
+# Python node executed as main process
 if __name__ == '__main__':
     try:
-        # Initializes a rospy node to let the SimpleActionClient publish and subscribe
-        rospy.init_node('movebase_client_py')
-        points_seq = [-9.1, -1.2, 10.7, 10.5,
-                      12.6, -1.7, 18.2, -1.4, -2.0, 4.0]
-
-        for i in [0, 2, 4, 6, 8]:
-            result = movebase_client(points_seq[i], points_seq[i+1])
+        # Assigning the waypoints to the Ebot mentioned in Task's problem statement
+        result = waypoints()
         if result:
             rospy.loginfo("Goal execution done!")
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
+

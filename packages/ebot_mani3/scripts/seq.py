628b995b5f742a65da392724e1611368e5f5a3a2
akhiljayan29aj <akhiljayan29.aj@gmail.com>
2021-01-31 00:17:05 +0530

    ebot_mani3 updated

--- packages/ebot_mani3/scripts/seq.py
+++ packages/ebot_mani3/scripts/seq.py
@@ -140,7 +140,7 @@ class Ur5Moveit:
             '\033[94m' + "Object of class Ur5Moveit Deleted." + '\033[0m')
 
 
-def client_coordinates(x, y, z, w):
+def client_coordinates(x, y, ori):
 
     # Initializing action client "move_base" with action definition file "MoveBaseAction"
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
@@ -158,8 +158,8 @@ def client_coordinates(x, y, z, w):
     goal.target_pose.pose.position.y = y
     goal.target_pose.pose.orientation.x = 0.0
     goal.target_pose.pose.orientation.y = 0.0
-    goal.target_pose.pose.orientation.z = z
-    goal.target_pose.pose.orientation.w = w
+    goal.target_pose.pose.orientation.z = ori[0]
+    goal.target_pose.pose.orientation.w = ori[1]
 
     # Sending the goal to the action server.
     client.send_goal(goal)
@@ -181,6 +181,14 @@ if __name__ == '__main__':
 
         ur5 = Ur5Moveit()
 
+        north = [0.0, 1.0]
+        south = [1.0, 0.0]
+        east = [-0.7, 0.7]
+        west = [0.7, 0.7]
+        northeast = [-0.289, 0.957]
+        southwest = [0.949, 0.315]
+
+        # Arm pose for better movement
         movearm = [math.radians(-22),
                    math.radians(-25),
                    math.radians(20),
@@ -188,25 +196,77 @@ if __name__ == '__main__':
                    math.radians(-29),
                    math.radians(-17)]
 
-        room1 = geometry_msgs.msg.Pose()
-        room1.position.x = 0.111409
-        room1.position.y = 0.020832
-        room1.position.z = 1.077843
-        room1.orientation.x = -0.65130811
-        room1.orientation.y = -0.73825586
-        room1.orientation.z = 0.11872579
-        room1.orientation.w = 0.12915193
+        pantry = [math.radians(172),
+                  math.radians(-43),
+                  math.radians(-84),
+                  math.radians(228),
+                  math.radians(-254),
+                  math.radians(169)]
 
         ur5.set_joint_angles(movearm)
 
-        # # Assigning the waypoints to the Ebot mentioned in Task's problem statement
-        # client_coordinates(12.93, 2.21, -0.709, 0.70)
-        client_coordinates(12.93, 1.17, -0.709, 0.70)
-        client_coordinates(14.23, -0.75, 0.0, 1.0)
+        # # pantry outside
+        client_coordinates(12.96, 1.27, east)
 
-        ur5.go_to_pose(room1)
-        client_coordinates(14.23, -0.75, 1.0, 0.0)
-        client_coordinates(11.5, -1.13, 1.0, 0.0)
+        # # pantry inside
+        client_coordinates(13.12, -1.0, north)
+
+        # # pantry left table
+        client_coordinates(14.4, -1.0, north)
+
+        ur5.set_joint_angles(pantry)
+
+        # rospy.sleep(10)
+
+        # pantry left table turn toward south
+        # client_coordinates(14.4, -1.0, south)
+
+        # # pantry right table
+        # client_coordinates(11.4, -1.0, south)
+
+        # rospy.sleep(10)
+
+        # # # pantry right table turn toward north
+        # client_coordinates(11.6, -1.0, north)
+
+        # # # # pantry inside
+        # client_coordinates(13.12, -1.0, west)
+
+        # # # # pantry outside
+        # client_coordinates(12.96, 1.27, south)
+
+        # # # # meeting outside
+        # client_coordinates(8.57, 1.09, west)
+
+        # # # # # meeting inside
+        # client_coordinates(8.58, 2.71, south)
+
+        # # # # # meeting drop box
+        # client_coordinates(7.4, 2.71, north)
+
+        # # # # meeting inside
+        # client_coordinates(8.58, 2.71, east)
+
+        # # # # meeting outside
+        # client_coordinates(8.57, 1.09, north)
+
+        # # research
+        # client_coordinates(10.6, 9.48, east)
+
+        # # store
+        # client_coordinates(25.8, -3.05, northeast)
+
+        # # store return
+        # client_coordinates(25.8, -3.05, southwest)
+
+        # # last room outside
+        # client_coordinates(5.19, 0.85, east)
+
+        # # last room inside v1
+        # client_coordinates(5.6, -0.67, east)
+
+        # # last room inside v2
+        # client_coordinates(4.93, -1.18, east)
 
     except rospy.ROSInterruptException:
         rospy.loginfo("Navigation test finished.")
